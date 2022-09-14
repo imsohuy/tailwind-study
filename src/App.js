ed0b86524939a67a897bdafd0d1e0e07fcd42886
imsohuy <nguyenhuuhuy203@gmail.com>
2022-09-14 23:51:30 +0700

    init repo

--- src/App.js
+++ src/App.js
@@ -1,24 +1,160 @@
-import logo from './logo.svg';
-import './App.css';
+/* eslint-disable jsx-a11y/anchor-is-valid */
+import './App.css'
+
+import intro from './assets/images/illustration-intro.svg'
+import logo from './assets/images/logo.svg'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <nav className="relative container mx-auto p-6">
+        <div className="flex items-center justify-between">
+          <div class="pt-2">
+            <img src={logo} alt="React Logo" />
+          </div>
+          <div className="hidden md:flex space-x-12">
+            <a href="#" class="hover:text-darkGrayishBlue">
+              Pricing
+            </a>
+            <a href="#" class="hover:text-darkGrayishBlue">
+              About us
+            </a>
+            <a href="#" class="hover:text-darkGrayishBlue">
+              Careers
+            </a>
+            <a href="#" class="hover:text-darkGrayishBlue">
+              Community
+            </a>
+            <a href="#" class="hover:text-darkGrayishBlue">
+              Product
+            </a>
+          </div>
+          <a
+            href=""
+            className="hidden md:block p-3 px-6 pt-2 text-white bg-brightRed rounded-full baseline hover:bg-brightRedLight"
+          >
+            Get started
+          </a>
+        </div>
+      </nav>
+
+      <secion id="hero">
+        <div className="container flex flex-col-reverse md:flex-row items-center px-6 mx-auto mt-10 space-y-0 md:space-y-0">
+          <div className="flex flex-col mb-32 space-y-12 md:w-1/2">
+            <h1 className="max-w-md text-4xl font-bold text-center md:text-5xl md:text-left">
+              Bring everyone together to build better products
+            </h1>
+            <p className="max-w-sm text-center text-darkGrayishBlue md:text-left">
+              Manage makes it simple for software teams to plan day-to-day tasks
+              while keeping the larger team goals in views.
+            </p>
+            <div className="flex justify-center md:justify-start">
+              <a
+                href=""
+                className="hidden md:block p-3 px-6 pt-2 text-white bg-brightRed rounded-full baseline hover:bg-brightRedLight"
+              >
+                Get started
+              </a>
+            </div>
+          </div>
+          <div className="md:w-1/2">
+            <img src={intro} alt="intro" />
+          </div>
+        </div>
+      </secion>
+      <section id="features">
+        {/* flex container */}
+        <div className="container flex flex-col px-4 mx-auto mt-10 space-y-12 md:space-y-0 md:flex-row">
+          {/* what different */}
+          <div className="flex flex-col space-y-12 md:w-1/2">
+            <h2 className="max-w-md text-4xl font-bold text-center md:text-left">
+              What's different about Manage?
+            </h2>
+            <p className="max-w-sm text-center text-darkGrayishBlue md:text-left">
+              Anim labore dolor in pariatur. Aute ex cupidatat labore pariatur.
+              Magna ut officia irure ex proident. Do commodo nisi officia cillum
+              consectetur proident culpa mollit commodo veniam aliqua.
+            </p>
+          </div>
+          {/* numbered list */}
+          <div className="flex flex-col space-y-8 md:w-1/2">
+            {/* list item 1 */}
+            <div className="flex flex-col space-y-3 md:space-y-0 md:space-x-6 md:flex-row">
+              {/* Heading */}
+              <div className="rounded-l-full bg-brightRedSupLight md:bg-transparent">
+                <div className="flex items-center space-x-2">
+                  <div className="px-4 py-2 text-white rounded-full md:py-1 bg-brightRed">
+                    01
+                  </div>
+                  <h3 className="text-base font-bold md:mb-4 md:hidden">
+                    Track company-wide progress
+                  </h3>
+                </div>
+              </div>
+              <div>
+                <h3 className="hidden mb-4 text-lg font-bold md:block">
+                  Track company-wide progress
+                </h3>
+                <p3 className="text-darkGrayishBlue">
+                  See how your day-to-day tasks fit into the wider vision. Go
+                  from tracking progress at the milestone level all the way down
+                  to the smallest of details. Never lose sight of the bigger
+                  picture again.
+                </p3>
+              </div>
+            </div>
+            {/* list item 2 */}
+            <div className="flex flex-col space-y-3 md:space-y-0 md:space-x-6 md:flex-row">
+              {/* Heading */}
+              <div className="rounded-l-full bg-brightRedSupLight md:bg-transparent">
+                <div className="flex items-center space-x-2">
+                  <div className="px-4 py-2 text-white rounded-full md:py-1 bg-brightRed">
+                    02
+                  </div>
+                  <h3 className="text-base font-bold md:mb-4 md:hidden">
+                    Advanced built-in reports
+                  </h3>
+                </div>
+              </div>
+              <div>
+                <h3 className="hidden mb-4 text-lg font-bold md:block">
+                  Advanced built-in reports
+                </h3>
+                <p3 className="text-darkGrayishBlue">
+                  Set internal delivery estimates and track progress toward
+                  company goals. Our customisable dashboard helps you build out
+                  the reports you need to keep key stakeholders informed.
+                </p3>
+              </div>
+            </div>
+            {/* list item 3 */}
+            <div className="flex flex-col space-y-3 md:space-y-0 md:space-x-6 md:flex-row">
+              {/* Heading */}
+              <div className="rounded-l-full bg-brightRedSupLight md:bg-transparent">
+                <div className="flex items-center space-x-2">
+                  <div className="px-4 py-2 text-white rounded-full md:py-1 bg-brightRed">
+                    03
+                  </div>
+                  <h3 className="text-base font-bold md:mb-4 md:hidden">
+                    Everything you need in one place
+                  </h3>
+                </div>
+              </div>
+              <div>
+                <h3 className="hidden mb-4 text-lg font-bold md:block">
+                  Everything you need in one place
+                </h3>
+                <p3 className="text-darkGrayishBlue">
+                  Stop jumping from one service to another to communicate, store
+                  files, track tasks and share documents. Manage offers an
+                  all-in-one team productivity solution.
+                </p3>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+    </>
   );
 }
 
