3f56c22a6078d79410f8d58f9fd99a12353faaa6
imsohuy <nguyenhuuhuy203@gmail.com>
2022-09-15 00:15:20 +0700

    add manager section

--- src/App.js
+++ src/App.js
@@ -1,6 +1,9 @@
 /* eslint-disable jsx-a11y/anchor-is-valid */
 import './App.css'
 
+import avatarAnisa from './assets/images/avatar-anisha.png'
+import avatarRichard from './assets/images/avatar-richard.png'
+import avatarShanai from './assets/images/avatar-shanai.png'
 import intro from './assets/images/illustration-intro.svg'
 import logo from './assets/images/logo.svg'
 
@@ -151,6 +154,57 @@ function App() {
                 </p3>
               </div>
             </div>
+          </div>
+        </div>
+      </section>
+      {/* Testiomonials */}
+      <section id="testiomonials">
+        {/* container to heading and teams blocks */}
+        <div className="max-w-6xl px-5 mx-auto mt-32 text-center">
+          {/* Heading */}
+          <h2 className="text-4xl font-bold text-center">
+            What's Different About Manage?
+          </h2>
+          {/* Testimonials Container */}
+          <div className="flex flex-col mt-24 md:flex-row md:space-x-6">
+            {/* Testimonial 1 */}
+            <div className="flex flex-col items-center p-6 space-y-6 rounded-lg bg-veryLightGray md:w-1/3">
+              <img src={avatarAnisa} alt="" className="w-16 -mt-14" />
+              <h5 className="text-lg font-bold">Anisha Li</h5>
+              <p className="text-sm text-darkGrayishBlue">
+                “Manage has supercharged our team’s workflow. The ability to
+                maintain visibility on larger milestones at all times keeps
+                everyone motivated.”
+              </p>
+            </div>
+            {/* Testimonial 2 */}
+            <div className="hidden flex-col items-center p-6 space-y-6 rounded-lg bg-veryLightGray md:flex md:w-1/3">
+              <img src={avatarRichard} alt="" className="w-16 -mt-14" />
+              <h5 className="text-lg font-bold">Lalisa Monoban</h5>
+              <p className="text-sm text-darkGrayishBlue">
+                “We have been able to cancel so many other subscriptions since
+                using Manage. There is no more cross-channel confusion and
+                everyone is much more focused.”
+              </p>
+            </div>
+            {/* Testimonial 3 */}
+            <div className="hidden flex-col items-center p-6 space-y-6 rounded-lg bg-veryLightGray md:flex md:w-1/3">
+              <img src={avatarShanai} alt="" className="w-16 -mt-14" />
+              <h5 className="text-lg font-bold">Ali Bravo</h5>
+              <p className="text-sm text-darkGrayishBlue">
+                “Sit amet aute irure officia consectetur mollit qui. Nulla
+                labore eiusmod exercitation culpa enim deserunt do quis ea Lorem
+                minim. Magna qui elit adipisicing occaecat deserunt.”
+              </p>
+            </div>
+          </div>
+          <div className="my-16">
+            <a
+              href=""
+              className="p-3 px-6 pt-2 text-white bg-brightRed rounded-full baseline hover:bg-brightRedLight"
+            >
+              Get started
+            </a>
           </div>
         </div>
       </section>
